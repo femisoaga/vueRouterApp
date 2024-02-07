5da13434b85167722f0910c31d82cb4853951fdd
Samuel Soaga <your-email-address@example.com>
2024-02-07 14:33:22 +0100

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
+      pariatur dolorum delectus veritatis eaque nesciunt tempore deserunt dolore
+      ut quis, explicabo a facere, molestias aliquid asperiores velit alias,
+      nobis possimus in culpa nisi praesentium provident. Eaque totam dolorem
+      itaque, vitae possimus reiciendis, amet, praesentium voluptate sed officia
+      libero officiis. Obcaecati laboriosam maxime voluptatum minus adipisci
+      mollitia eius quos a repudiandae minima officiis quaerat aperiam explicabo
+      labore libero velit distinctio laudantium ipsum, esse corporis repellat
+      quod fugiat. Architecto, sed qui perspiciatis aliquid beatae consectetur
+      incidunt voluptatibus inventore soluta. Veniam voluptates iste quidem
+      omnis nisi asperiores, nulla ab, unde neque porro maxime.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
+      pariatur dolorum delectus veritatis eaque nesciunt tempore deserunt dolore
+      ut quis, explicabo a facere, molestias aliquid asperiores velit alias,
+      nobis possimus in culpa nisi praesentium provident. Eaque totam dolorem
+      itaque, vitae possimus reiciendis, amet, praesentium voluptate sed officia
+      libero officiis. Obcaecati laboriosam maxime voluptatum minus adipisci
+      mollitia eius quos a repudiandae minima officiis quaerat aperiam explicabo
+      labore libero velit distinctio laudantium ipsum, esse corporis repellat
+      quod fugiat. Architecto, sed qui perspiciatis aliquid beatae consectetur
+      incidunt voluptatibus inventore soluta. Veniam voluptates iste quidem
+      omnis nisi asperiores, nulla ab, unde neque porro maxime.
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
---
+++ src/views/jobs/JobsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id">
+      <h2>{{ job.title }}</h2>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
+  },
+};
+</script>
+
+<style>
+</style>
